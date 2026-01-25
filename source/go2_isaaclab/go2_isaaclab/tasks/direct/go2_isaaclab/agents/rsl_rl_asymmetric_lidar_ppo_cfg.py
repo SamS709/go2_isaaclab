a26df52b19c34904909b829da1239629f17e4c32
Sami <sami.leroux75@mail.com>
2026-01-25 13:58:15 +0100

    lidar env + clean code

--- source/go2_isaaclab/go2_isaaclab/tasks/direct/go2_isaaclab/agents/rsl_rl_asymmetric_lidar_ppo_cfg.py
+++ source/go2_isaaclab/go2_isaaclab/tasks/direct/go2_isaaclab/agents/rsl_rl_asymmetric_lidar_ppo_cfg.py
@@ -10,10 +10,38 @@ from isaaclab_rl.rsl_rl import RslRlPpoActorCriticRecurrentCfg, RslRlOnPolicyRun
 
 @configclass
 class Go2AsymmetricPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    # ---------------PUT THAT NEXT----------------------------------
+    # num_steps_per_env = 24
+    # max_iterations = 1500
+    # save_interval = 50
+    # experiment_name = "unitree_go2_rough"
+    # policy = RslRlPpoActorCriticCfg(
+    #     init_noise_std=1.0,
+    #     actor_obs_normalization=False,
+    #     critic_obs_normalization=False,
+    #     actor_hidden_dims=[512, 256, 128],
+    #     critic_hidden_dims=[512, 256, 128],
+    #     activation="elu",
+    # )
+    # algorithm = RslRlPpoAlgorithmCfg(
+    #     value_loss_coef=1.0,
+    #     use_clipped_value_loss=True,
+    #     clip_param=0.2,
+    #     entropy_coef=0.01,
+    #     num_learning_epochs=5,
+    #     num_mini_batches=4,
+    #     learning_rate=1.0e-3,
+    #     schedule="adaptive",
+    #     gamma=0.99,
+    #     lam=0.95,
+    #     desired_kl=0.01,
+    #     max_grad_norm=1.0,
+    # )
+    # ------------------------------------------------
     num_steps_per_env = 24
     max_iterations = 50000
     save_interval = 100
-    experiment_name = "go2_asymmetric"
+    experiment_name = "go2_asymmetric_lidar"
     empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
